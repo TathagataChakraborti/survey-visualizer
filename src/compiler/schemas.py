ef9f049d0f02511c4057b9ee84edc0f5d67f0bf9
Tathagata Chakraborti <TathagataChakraborti@users.noreply.github.com>
2023-09-30 23:44:13 -0400

    New deployments (#41)
    * carbon v11 migration checkin
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * landing page
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * basic element
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * taxonomy page
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * insights + network tab
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * affinity tab
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * compiler fix
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * vamhri
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * vamhri-2
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * vamhri-3
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * vamhri-4
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * vamhri-5
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * vamhri-final
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * trigger heroku again
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * move to firebase
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * adding config files back in
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * migration complete
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * Update README.md
    
    * cache
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    * merge
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
    
    ---------
    
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>

--- src/compiler/schemas.py
+++ src/compiler/schemas.py
@@ -1,31 +1,33 @@
-from typing import List, Union, Dict, TypedDict
+from typing import List, Union, Optional
+from pydantic import BaseModel
 
 
-class Paper(TypedDict):
-    UID: int
-    slug: str
-    title: str
-    abstract: str
-    authors: Union[str, List[str]]
-    venue: str
-    sessions: str
-    year: int
-    keywords: Union[str, List[str]]
-    tags: List[str]
-    citations: List[int]
+class Paper(BaseModel):
+    UID: int = 0
+    slug: str = ""
+    title: str = ""
+    abstract: str = ""
+    link: str = ""
+    authors: Union[str, List[str]] = ""
+    venue: str = ""
+    sessions: str = ""
+    year: int = 0
+    keywords: Union[str, List[str]] = ""
+    tags: List[str] = []
+    citations: List[int] = []
     selected: bool = True
 
 
-class Element(TypedDict):
+class Element(BaseModel):
     name: str
     expanded: bool = True
-    parent: str = None
+    parent: Optional[str] = None
     level: int
     start: int
     stop: int
 
 
-class Taxonomy(TypedDict):
+class Taxonomy(BaseModel):
     name: str
     taxonomy: List[List[Element]]
     data: List[Paper]
@@ -35,19 +37,17 @@ class Node(Paper):
     group: str
 
 
-class Link(TypedDict):
-    source: str
-    target: str
-    value: int
+class Link(BaseModel):
+    source: int
+    target: int
 
 
-class Network(TypedDict):
+class Network(BaseModel):
     nodes: List[Node]
     links: List[Link]
-    group_map: Dict
 
 
-class Embedding(TypedDict):
+class Embedding(BaseModel):
     UID: int
     x: float
     y: float
--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -3,205 +3,230 @@ import GitHubButton from 'react-github-btn';
 import { Document, LogoGithub, LogoSlack, Add } from '@carbon/icons-react';
 import { BasicElement } from '../../components/BasicElement';
 import {
-  Grid,
-  Column,
-  Button,
-  ButtonSet,
-  ContentSwitcher,
-  Switch,
-  CodeSnippet,
-  Link,
-  Tile,
-  ContainedList,
-  ContainedListItem,
+    Grid,
+    Column,
+    Button,
+    ButtonSet,
+    ContentSwitcher,
+    Switch,
+    CodeSnippet,
+    Link,
+    Tile,
+    ContainedList,
+    ContainedListItem,
 } from '@carbon/react';
 
-let config = require('../../config.json');
+import config from '../../config.json';
 
 class LandingPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      view: config.default_view,
+    constructor(props) {
+        super(props);
+        this.state = {
+            view: config.default_view,
+        };
+    }
+
+    logChange = e => {
+        this.setState({
+            view: e.name,
+        });
     };
-  }
 
-  logChange = e => {
-    this.setState({
-      view: e.name,
-    });
-  };
+    render() {
+        return (
+            <>
+                <Grid>
+                    <Column lg={4} md={8} sm={4}>
+                        <div className="cds--container">
+                            <ContentSwitcher
+                                onChange={e => this.logChange(e)}
+                                size="sm"
+                                selectedIndex={config.views
+                                    .map(e => e.name)
+                                    .indexOf(this.state.view)}>
+                                {config.views
+                                    .filter(item => !item.disabled)
+                                    .map((view, id) => (
+                                        <Switch
+                                            key={id}
+                                            name={view.name}
+                                            text={view.name}
+                                        />
+                                    ))}
+                            </ContentSwitcher>
 
-  render() {
-    return (
-      <>
-        <Grid>
-          <Column lg={4} md={8} sm={4}>
-            <div className="cds--container">
-              <ContentSwitcher
-                onChange={e => this.logChange(e)}
-                size="sm"
-                selectedIndex={config.views
-                  .map(e => e.name)
-                  .indexOf(this.state.view)}>
-                {config.views.map((view, id) => (
-                  <Switch key={id} name={view.name} text={view.name} />
-                ))}
-              </ContentSwitcher>
+                            <br />
 
-              <br />
+                            <h3>{config.metadata.title_text}</h3>
 
-              <h3>{config.metadata.title_text}</h3>
+                            <br />
+                            <CodeSnippet type="multi">
+                                {config.metadata.citation_text}
+                            </CodeSnippet>
 
-              <br />
-              <CodeSnippet type="multi">
-                {config.metadata.citation_text}
-              </CodeSnippet>
+                            <br />
 
-              <br />
+                            <ButtonSet stacked>
+                                <Button
+                                    kind="primary"
+                                    className="buttonset"
+                                    size="sm"
+                                    renderIcon={Document}
+                                    href={config.metadata.primary_link}
+                                    target="_blank">
+                                    Read
+                                </Button>
+                                <br />
+                                {config.metadata.secondary_links.length ===
+                                    1 && (
+                                    <>
+                                        <Button
+                                            kind="tertiary"
+                                            className="buttonset"
+                                            size="sm"
+                                            renderIcon={Document}
+                                            href={
+                                                config.metadata
+                                                    .secondary_links[0].link
+                                            }
+                                            target="_blank">
+                                            See Also
+                                        </Button>
+                                        <br />
+                                    </>
+                                )}
+                                <Button
+                                    kind="tertiary"
+                                    className="buttonset tertiary-secondary"
+                                    renderIcon={LogoGithub}
+                                    size="sm"
+                                    href={config.metadata.link_to_contribute}
+                                    target="_blank">
+                                    Contribute
+                                </Button>
+                                <br />
+                                {config.metadata.community_link && (
+                                    <>
+                                        <Button
+                                            kind="tertiary"
+                                            className="buttonset tertiary-danger"
+                                            renderIcon={LogoSlack}
+                                            size="sm"
+                                            href={
+                                                config.metadata.community_link
+                                            }
+                                            target="_blank">
+                                            Community
+                                        </Button>
+                                        <br />
+                                    </>
+                                )}
+                            </ButtonSet>
 
-              <ButtonSet stacked>
-                <Button
-                  kind="primary"
-                  className="buttonset"
-                  size="sm"
-                  renderIcon={Document}
-                  href={config.metadata.primary_link}
-                  target="_blank">
-                  Read
-                </Button>
-                <br />
-                {config.metadata.secondary_links.length === 1 && (
-                  <>
-                    <Button
-                      kind="tertiary"
-                      className="buttonset"
-                      size="sm"
-                      renderIcon={Document}
-                      href={config.metadata.secondary_links[0].link}
-                      target="_blank">
-                      See Also
-                    </Button>
-                    <br />
-                  </>
-                )}
-                <Button
-                  kind="tertiary"
-                  className="buttonset tertiary-secondary"
-                  renderIcon={LogoGithub}
-                  size="sm"
-                  href={config.metadata.link_to_contribute}
-                  target="_blank">
-                  Contribute
-                </Button>
-                <br />
-                {config.metadata.community_link && (
-                  <>
-                    <Button
-                      kind="tertiary"
-                      className="buttonset tertiary-danger"
-                      renderIcon={LogoSlack}
-                      size="sm"
-                      href={config.metadata.community_link}
-                      target="_blank">
-                      Community
-                    </Button>
-                    <br />
-                  </>
-                )}
-              </ButtonSet>
+                            {config.metadata.info_tile && (
+                                <Tile>
+                                    <p style={{ fontSize: 'small' }}>
+                                        {config.metadata.info_text}
+                                    </p>
 
-              {config.metadata.info_tile && (
-                <Tile>
-                  <p style={{ fontSize: 'small' }}>
-                    {config.metadata.info_text}
-                  </p>
+                                    <div style={{ paddingTop: '3px' }}>
+                                        {config.metadata.info_link.map(
+                                            (link, id) => (
+                                                <span key={id}>
+                                                    {' '}
+                                                    {id > 0 && '|'}{' '}
+                                                    <Link
+                                                        href={link.link}
+                                                        target="_blank">
+                                                        {link.text}
+                                                    </Link>
+                                                </span>
+                                            )
+                                        )}
+                                    </div>
+                                </Tile>
+                            )}
 
-                  <div style={{ paddingTop: '3px' }}>
-                    {config.metadata.info_link.map((link, id) => (
-                      <span key={id}>
-                        {' '}
-                        {id > 0 && '|'}{' '}
-                        <Link href={link.link} target="_blank">
-                          {link.text}
-                        </Link>
-                      </span>
-                    ))}
-                  </div>
-                </Tile>
-              )}
+                            <br />
 
-              <br />
+                            {config.metadata.secondary_links.length > 1 && (
+                                <ContainedList
+                                    size="sm"
+                                    label="See Also"
+                                    kind="disclosed"
+                                    className="see-also">
+                                    {config.metadata.secondary_links.map(
+                                        (item, i) => (
+                                            <ContainedListItem
+                                                onClick={() =>
+                                                    window.open(item.link)
+                                                }
+                                                action={
+                                                    <Button
+                                                        href={item.link}
+                                                        target="_blank"
+                                                        kind="ghost"
+                                                        iconDescription="URL"
+                                                        hasIconOnly
+                                                        renderIcon={Add}
+                                                    />
+                                                }
+                                                key={i}>
+                                                {item.name}
+                                            </ContainedListItem>
+                                        )
+                                    )}
+                                </ContainedList>
+                            )}
+                        </div>
 
-              {config.metadata.secondary_links.length > 1 && (
-                <ContainedList
-                  size="sm"
-                  label="See Also"
-                  kind="disclosed"
-                  className="see-also">
-                  {config.metadata.secondary_links.map((item, i) => (
-                    <ContainedListItem
-                      onClick={() => window.open(item.link)}
-                      action={
-                        <Button
-                          href={item.link}
-                          target="_blank"
-                          kind="ghost"
-                          iconDescription="URL"
-                          hasIconOnly
-                          renderIcon={Add}
+                        <div className="footer">
+                            <div className="cds--container">
+                                <p
+                                    style={{
+                                        fontSize: 'small',
+                                        marginBottom: '10px',
+                                        maxWidth: '75%',
+                                    }}>
+                                    Follow us on GitHub. Your love keeps us
+                                    going!{' '}
+                                    <span role="img" aria-label="hugging face">
+                                        &#129303;
+                                    </span>
+                                </p>
+
+                                <GitHubButton
+                                    href={config.metadata.link_to_code}
+                                    data-size="small"
+                                    data-show-count="true"
+                                    aria-label="Star survey-visualizer on GitHub">
+                                    Star
+                                </GitHubButton>
+                            </div>
+
+                            <div
+                                className="cds--container"
+                                style={{ paddingTop: '0' }}>
+                                App built by{' '}
+                                <Link
+                                    href="https://twitter.com/tchakra2"
+                                    target="_blank">
+                                    tchakra2
+                                </Link>
+                            </div>
+                        </div>
+                    </Column>
+
+                    <Column lg={12} md={8} sm={4}>
+                        <BasicElement
+                            props={this.state.view}
+                            logChange={this.logChange.bind(this)}
                         />
-                      }
-                      key={i}>
-                      {item.name}
-                    </ContainedListItem>
-                  ))}
-                </ContainedList>
-              )}
-            </div>
-
-            <div className="footer">
-              <div className="cds--container">
-                <p
-                  style={{
-                    fontSize: 'small',
-                    marginBottom: '10px',
-                    maxWidth: '75%',
-                  }}>
-                  Follow us on GitHub. Your love keeps us going!{' '}
-                  <span role="img" aria-label="hugging face">
-                    &#129303;
-                  </span>
-                </p>
-
-                <GitHubButton
-                  href={config.metadata.link_to_code}
-                  data-size="small"
-                  data-show-count="true"
-                  aria-label="Star survey-visualizer on GitHub">
-                  Star
-                </GitHubButton>
-              </div>
-
-              <div className="cds--container" style={{ paddingTop: '0' }}>
-                App built by{' '}
-                <Link href="https://twitter.com/tchakra2" target="_blank">
-                  tchakra2
-                </Link>
-              </div>
-            </div>
-          </Column>
-
-          <Column lg={12} md={8} sm={4}>
-            <BasicElement
-              props={this.state.view}
-              logChange={this.logChange.bind(this)}
-            />
-          </Column>
-        </Grid>
-      </>
-    );
-  }
+                    </Column>
+                </Grid>
+            </>
+        );
+    }
 }
 
 export default LandingPage;
